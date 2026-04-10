2d00addf036f15446e81aeb4ce7637b0042a7add
james.m.birchfield <james.m.birchfield@enlighten.com>
2026-04-09 17:02:38 -0700

    feat(airflow): Add insecure option to disable TLS verification
    - Add an `insecure` option to the AirflowConfig struct.
    - When `insecure` is set to true, TLS certificate verification is disabled for the Airflow client.
    - Add a warning log message when `insecure` is set to true to inform the user that TLS verification is disabled.

--- crates/flowrs-airflow/src/client/base.rs
+++ crates/flowrs-airflow/src/client/base.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use log::debug;
+use log::{debug, warn};
 use reqwest::{Method, Url};
 use std::convert::TryFrom;
 use std::fmt;
@@ -28,6 +28,12 @@ impl fmt::Debug for BaseClient {
 
 impl BaseClient {
     pub fn new(config: AirflowConfig) -> Result<Self> {
+        if config.insecure {
+            warn!(
+                "TLS certificate verification is disabled for server '{}'",
+                config.name
+            );
+        }
         let client = reqwest::Client::builder()
             .timeout(Duration::from_secs(config.timeout_secs))
             .use_rustls_tls()
